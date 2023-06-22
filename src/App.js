602bf0da235bfc6b6a5943484126feaa84f80604
varshithsamineni <114898379+varshithsamineni@users.noreply.github.com>
2023-06-23 00:42:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './styles.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const add = (e) => {
+    e.preventDefault();
+    setItems([...items, { id: uuidv4(), newItem }]);
+    setNewItem("");
+  }
+
+  const deleteItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+        </div>
+        <button className="btn" onClick={add}>Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {items.map((item) => (
+          <li key={item.id}>
+            <label>
+              <input type="checkbox" checked={item.completed} />{item.newItem}
+            </label>
+            <button className="btn btn-danger" onClick={() => deleteItem(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
